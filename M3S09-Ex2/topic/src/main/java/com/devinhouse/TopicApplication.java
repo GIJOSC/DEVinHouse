7b06f9a35a6900eda700ecd2b3de9ad8d1083636
Gesiel Neves <neves.gesiel@gmail.com>
2022-05-14 11:41:12 -0300

    Adicionado mais exercícios

---
+++ M3S09-Ex2/topic/src/main/java/com/devinhouse/TopicApplication.java
@@ -0,0 +1,13 @@
+package com.devinhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicApplication.class, args);
+	}
+
+}
